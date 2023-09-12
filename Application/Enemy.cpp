c4dc5e8da2e2a6913c21adf42f37f3acdd5962a8
晴彦 相川 <k021g1126@g.neec.ac.jp>
2023-09-12 14:08:36 +0900

    取り残された

--- Application/Enemy.cpp
+++ Application/Enemy.cpp
@@ -144,7 +144,7 @@ void Enemy::Draw(void)
 		// 縮み状態なら
 		if (frameCount_move_ == 0)
 		{
-			// 敵の色は赤色に
+			// 敵の色は通常色に
 			DrawBoxSprite(png_enemy_, { 1.0f,1.0f,1.0f,1.0f }, { 0.5f,0.5f });
 
 			/*DrawCircle((int32_t)position_.x, (int32_t)position_.y, (int32_t)radius_.x, UtilL::Color::WHITE, false, 1);*/
