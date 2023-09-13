1925fa548f42884a7307d9c3c12a9085bfda6b6b
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-09-13 19:39:26 +0900

    いったんプッシュ。リザルトシーンの表示とか追加。タイトルからのとび先をDebugだとResultへ、ReleaseだとTutorialへ。 チュートリアル内でのリトライはチュートリアルへ帰るように

--- Application/Base/ResultScene.cpp
+++ Application/Base/ResultScene.cpp
@@ -13,17 +13,17 @@ void ResultScene::Finalize()
 
 void ResultScene::Initialize(void)
 {
-	//画像読み込み
-	png_result_ = TextureManager::LoadGraph("result.png");
-	//BGM再生
-	Sound::GetInstance().PlayWave("result_BGM.wav", 0.2f, true);
+    //画像読み込み
+    png_result_ = TextureManager::LoadGraph("result.png");
+    //BGM再生
+    Sound::GetInstance().PlayWave("result_BGM.wav", 0.2f, true);
 
-	Score::GetInstance()->SaveScore();
+    Score::GetInstance()->SaveScore();
 
     UI::GetInstance()->SetPos(UIType::NowScore, { 250.f,350.f });
     UI::GetInstance()->SetSize(UIType::NowScore, 0.5f);
-    Score::GetInstance()->SetNowPos({1000.f,350.f});
-	Score::GetInstance()->SetNowSize(0.5f);
+    Score::GetInstance()->SetNowPos({ 1000.f,350.f });
+    Score::GetInstance()->SetNowSize(0.5f);
     Score::GetInstance()->SetNowNum();
     Score::GetInstance()->SetRank();
 
@@ -33,9 +33,29 @@ void ResultScene::Initialize(void)
     Score::GetInstance()->SetHighSize(0.5f);
     Score::GetInstance()->SetHighNum();
 
-	UI::GetInstance()->SetPos(UIType::Abutton, { 640.f,600.f });
-	UI::GetInstance()->SetSize(UIType::Abutton, 0.8f);
-	UI::GetInstance()->SetAncorPoint(UIType::Abutton, {0.5f,0.5f});
+
+
+    UI::GetInstance()->SetPos(UIType::Retry, { 420,600 });
+    UI::GetInstance()->SetAncorPoint(UIType::Retry, { 0.5f,0.5f });
+    UI::GetInstance()->SetPos(UIType::ToTitle, { 940,600 });
+    UI::GetInstance()->SetAncorPoint(UIType::ToTitle, { 0.5f,0.5f });
+    UI::GetInstance()->SetPos(UIType::Abutton, { 420,680 });
+    UI::GetInstance()->SetSize(UIType::Abutton, 0.3f);
+    UI::GetInstance()->SetAncorPoint(UIType::Abutton, { 0.5f,0.5f });
+    if (destination_ == Destination::RETRY)
+    {
+        UI::GetInstance()->SetColor(UIType::ToTitle, { 1,1,1,0.3f });
+        UI::GetInstance()->SetSize(UIType::ToTitle, 0.4f);
+        UI::GetInstance()->SetColor(UIType::Retry, { 1,1,1,1 });
+        UI::GetInstance()->SetSize(UIType::Retry, 0.7f);
+    }
+    else if (destination_ == Destination::TITLE)
+    {
+        UI::GetInstance()->SetColor(UIType::Retry, { 1,1,1,0.3f });
+        UI::GetInstance()->SetSize(UIType::Retry, 0.4f);
+        UI::GetInstance()->SetColor(UIType::ToTitle, { 1,1,1,1 });
+        UI::GetInstance()->SetSize(UIType::ToTitle, 0.7f);
+    }
 }
 
 void ResultScene::Update(void)
@@ -48,6 +68,11 @@ void ResultScene::Update(void)
         }
         destination_++;
         destination_ = (std::min)(destination_, 1);
+        UI::GetInstance()->SetColor(UIType::Retry, { 1,1,1,0.3f });
+        UI::GetInstance()->SetSize(UIType::Retry, 0.4f);
+        UI::GetInstance()->SetColor(UIType::ToTitle, { 1,1,1,1 });
+        UI::GetInstance()->SetSize(UIType::ToTitle, 0.7f);
+        UI::GetInstance()->SetPos(UIType::Abutton, { 920,680 });
     }
     else if (PadInput::GetInstance().GetLeftStickTilt().x <= -0.3f)
     {
@@ -57,6 +82,11 @@ void ResultScene::Update(void)
         }
         destination_--;
         destination_ = (std::max)(destination_, 0);
+        UI::GetInstance()->SetColor(UIType::ToTitle, { 1,1,1,0.3f });
+        UI::GetInstance()->SetSize(UIType::ToTitle, 0.4f);
+        UI::GetInstance()->SetColor(UIType::Retry, { 1,1,1,1 });
+        UI::GetInstance()->SetSize(UIType::Retry, 0.7f);
+        UI::GetInstance()->SetPos(UIType::Abutton, { 420,680 });
     }
 
     if (destination_ == Destination::RETRY)
@@ -89,6 +119,187 @@ void ResultScene::Update(void)
         SceneManager::GetInstance().SetNextScene(SceneFactory::Usage::TITLE);
     }
 #endif // _DEBUG
+
+
+    //if (progressNum_ == 1)
+    //{
+    //    // 透明化しはじめるまでのフレームを待つため、kMaxFrame4StartTrans_の値を超えるまでは、rateの計算が出来ないようにする
+    //    float frame = (float)(std::max)(frameCount_sign_ - kMaxFrame4StartTrans_, 0);
+
+    //    // 現在フレームと最大値の割合算出
+    //    float rate = (std::min)(frame / kMaxFrameTransColor_, 1.f);
+    //    // 割合から、α値を段々減算
+    //    alpha_sign_ = (int32_t)((1 - rate) * 255);
+
+    //    // いったん仮でRB押しで次へ進行
+    //    if (PadInput::GetInstance().GetTriggerButton(GAMEPAD_RIGHT_SHOULDER))
+    //    {
+    //        // 看板用フレームカウンタ加算
+    //        frameCount_sign_++;
+    //    }
+
+    //    // フレームカウンタが起動されてたら加算し続ける
+    //    if (frameCount_sign_)
+    //    {
+    //        // 看板用フレームカウンタ加算
+    //        frameCount_sign_++;
+
+    //        // フレームカウンタの値が、(透明化し始めるまで + 透明になるまで + 次の間隔)を上回ったら
+    //        if (frameCount_sign_ > kMaxFrame4StartTrans_ + kMaxFrameTransColor_ + kIntervalNextSign_)
+    //        {
+    //            // カウンタ初期化して
+    //            frameCount_sign_ = 0;
+    //            // α値初期化して
+    //            alpha_sign_ = 255;
+    //            // 表示する看板を次のものへ
+    //            progressNum_++;
+    //        }
+    //    }
+    //}
+    //else if (progressNum_ == 2)
+    //{
+    //    // 透明化しはじめるまでのフレームを待つため、kMaxFrame4StartTrans_の値を超えるまでは、rateの計算が出来ないようにする
+    //    float frame = (float)(std::max)(frameCount_sign_ - kMaxFrame4StartTrans_, 0);
+
+    //    // 現在フレームと最大値の割合算出
+    //    float rate = (std::min)(frame / kMaxFrameTransColor_, 1.f);
+    //    // 割合から、α値を段々減算
+    //    alpha_sign_ = (int32_t)((1 - rate) * 255);
+
+    //    // いったん仮でA押し && スティック入力してたら
+    //    if (PadInput::GetInstance().GetPushButton(GAMEPAD_A) && PadInput::GetInstance().GetLeftStickTilt().x || PadInput::GetInstance().GetLeftStickTilt().y) // ここスローモーションかかるせいで時間かかりすぎ、エンジンならこうはならないと思うけど
+    //    {
+    //        // ここの加算は最初の一回のみにしたい
+    //        if (frameCount_sign_ == 0)
+    //        {
+    //            // 看板用フレームカウンタ加算
+    //            frameCount_sign_++;
+    //        }
+    //    }
+
+    //    // フレームカウンタが起動されてたら加算し続ける
+    //    if (frameCount_sign_)
+    //    {
+    //        // 看板用フレームカウンタ加算
+    //        frameCount_sign_++;
+
+    //        // フレームカウンタの値が、(透明化し始めるまで + 透明になるまで + 次の間隔)を上回ったら
+    //        if (frameCount_sign_ > kMaxFrame4StartTrans_ + kMaxFrameTransColor_ + kIntervalNextSign_)
+    //        {
+    //            // カウンタ初期化して
+    //            frameCount_sign_ = 0;
+    //            // α値初期化して
+    //            alpha_sign_ = 255;
+    //            // 表示する看板を次のものへ
+    //            progressNum_++;
+    //        }
+    //    }
+    //}
+    //else if (progressNum_ == 3)
+    //{
+    //    // 透明化しはじめるまでのフレームを待つため、kMaxFrame4StartTrans_の値を超えるまでは、rateの計算が出来ないようにする
+    //    float frame = (float)(std::max)(frameCount_sign_ - kMaxFrame4StartTrans_, 0);
+
+    //    // 現在フレームと最大値の割合算出
+    //    float rate = (std::min)(frame / kMaxFrameTransColor_, 1.f);
+    //    // 割合から、α値を段々減算
+    //    alpha_sign_ = (int32_t)((1 - rate) * 255);
+
+    //    // いったん仮でpad-AをReleaseで
+    //    if (PadInput::GetInstance().GetReleaseTrigger(GAMEPAD_A)) // ここの挙動ちょっと怪しい
+    //    {
+    //        // 看板用フレームカウンタ加算
+    //        frameCount_sign_++;
+    //    }
+
+    //    // フレームカウンタが起動されてたら加算し続ける
+    //    if (frameCount_sign_)
+    //    {
+    //        // 看板用フレームカウンタ加算
+    //        frameCount_sign_++;
+
+    //        // フレームカウンタの値が、(透明化し始めるまで + 透明になるまで + 次の間隔)を上回ったら
+    //        if (frameCount_sign_ > kMaxFrame4StartTrans_ + kMaxFrameTransColor_ + kIntervalNextSign_)
+    //        {
+    //            // カウンタ初期化して
+    //            frameCount_sign_ = 0;
+    //            // α値初期化して
+    //            alpha_sign_ = 255;
+    //            // 表示する看板を次のものへ
+    //            progressNum_++;
+    //        }
+    //    }
+    //}
+    //else if (progressNum_ == 4) // "8体くっつけてデカ団子に！"
+    //{
+    //    // 透明化しはじめるまでのフレームを待つため、kMaxFrame4StartTrans_の値を超えるまでは、rateの計算が出来ないようにする
+    //    float frame = (float)(std::max)(frameCount_sign_ - kMaxFrame4StartTrans_, 0);
+
+    //    // 現在フレームと最大値の割合算出
+    //    float rate = (std::min)(frame / kMaxFrameTransColor_, 1.f);
+    //    // 割合から、α値を段々減算
+    //    alpha_sign_ = (int32_t)((1 - rate) * 255);
+
+    //    // いったん仮でRB押しで次へ進行
+    //    if (PadInput::GetInstance().GetReleaseTrigger(GAMEPAD_RIGHT_SHOULDER))
+    //    {
+    //        // 看板用フレームカウンタ加算
+    //        frameCount_sign_++;
+    //    }
+
+    //    // フレームカウンタが起動されてたら加算し続ける
+    //    if (frameCount_sign_)
+    //    {
+    //        // 看板用フレームカウンタ加算
+    //        frameCount_sign_++;
+
+    //        // フレームカウンタの値が、(透明化し始めるまで + 透明になるまで + 次の間隔)を上回ったら
+    //        if (frameCount_sign_ > kMaxFrame4StartTrans_ + kMaxFrameTransColor_ + kIntervalNextSign_)
+    //        {
+    //            // カウンタ初期化して
+    //            frameCount_sign_ = 0;
+    //            // α値初期化して
+    //            alpha_sign_ = 255;
+    //            // 表示する看板を次のものへ
+    //            progressNum_++;
+    //        }
+    //    }
+    //}
+    //else if (progressNum_ == 5) // "倒すと高得点"
+    //{
+    //    // 透明化しはじめるまでのフレームを待つため、kMaxFrame4StartTrans_の値を超えるまでは、rateの計算が出来ないようにする
+    //    float frame = (float)(std::max)(frameCount_sign_ - kMaxFrame4StartTrans_, 0);
+
+    //    // 現在フレームと最大値の割合算出
+    //    float rate = (std::min)(frame / kMaxFrameTransColor_, 1.f);
+    //    // 割合から、α値を段々減算
+    //    alpha_sign_ = (int32_t)((1 - rate) * 255);
+
+    //    // いったん仮でRB押しで次へ進行
+    //    if (PadInput::GetInstance().GetReleaseTrigger(GAMEPAD_RIGHT_SHOULDER))
+    //    {
+    //        // 看板用フレームカウンタ加算
+    //        frameCount_sign_++;
+    //    }
+
+    //    // フレームカウンタが起動されてたら加算し続ける
+    //    if (frameCount_sign_)
+    //    {
+    //        // 看板用フレームカウンタ加算
+    //        frameCount_sign_++;
+
+    //        // フレームカウンタの値が、(透明化し始めるまで + 透明になるまで + 次の間隔)を上回ったら
+    //        if (frameCount_sign_ > kMaxFrame4StartTrans_ + kMaxFrameTransColor_ + kIntervalNextSign_)
+    //        {
+    //            // カウンタ初期化して
+    //            frameCount_sign_ = 0;
+    //            // α値初期化して
+    //            alpha_sign_ = 255;
+    //            // 表示する看板を次のものへ
+    //            progressNum_++;
+    //        }
+    //    }
+    //}
 }
 
 void ResultScene::Draw(void)
@@ -111,6 +322,9 @@ void ResultScene::DrawSprite2()
     UI::GetInstance()->Draw(UIType::NowScore);
     UI::GetInstance()->Draw(UIType::HighScore);
     UI::GetInstance()->Draw(UIType::Abutton);
+
+    UI::GetInstance()->Draw(UIType::Retry);
+    UI::GetInstance()->Draw(UIType::ToTitle);
 }
 
 void ResultScene::DrawImgui()
