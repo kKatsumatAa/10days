ac0c334c40f5debda6cb6a25d7602e34e9282155
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-09-13 22:59:38 +0900

    途中

--- Application/Base/ResultScene.h
+++ Application/Base/ResultScene.h
@@ -2,16 +2,26 @@
 #include "SceneState.h"
 #include "Object.h"
 #include <memory>
+#include "UtilL.h"
 
 class ResultScene :
     public SceneState
 {
 public:
+    // 定義
     enum Destination
     {
         RETRY,
         TITLE,
     };
+
+    // 定数
+    const int32_t kMaxFrame_wait4Start_{ 20 };
+    const int32_t kMaxFrame_currentScore_{ 80 };
+    const int32_t kMaxFrame_highScore_{ 80 };
+    const int32_t kMaxFrame_newRecord_{ 300 };
+    const int32_t kMaxFrame_DisplayRank_{ 300 };
+
 
     // 変数
     uint64_t png_result_ = 0;       //リザルト画像
@@ -19,15 +29,24 @@ public:
     uint32_t sceneChange_SE_ = 0;   //シーン遷移音
     int32_t destination_;
 
+
+    UtilL::NTimer timer_wait4Start_{(float)kMaxFrame_wait4Start_};
+    bool isEndAllDisplay_{}; // 全部の表示演出が終わったか
+    UtilL::NTimer timer_currentScore_{(float)kMaxFrame_currentScore_};
+    UtilL::NTimer timer_highScore_{(float)kMaxFrame_highScore_};
+    bool isNewRecord_{};
+    UtilL::NTimer timer_newRecord_{(float)kMaxFrame_newRecord_};
+    UtilL::NTimer timer_DisplayRank_{(float)kMaxFrame_DisplayRank_};
+
     Object result_;
 
     // 関数
-	void Finalize()override;
-	void Initialize()override;
-	void Update()override;
-	void Draw()override;
-	void DrawSprite()override;
+    void Finalize()override;
+    void Initialize()override;
+    void Update()override;
+    void Draw()override;
+    void DrawSprite()override;
     void DrawSprite2()override;
-	void DrawImgui()override;
+    void DrawImgui()override;
 };
 
--- Application/Others/Score.h
+++ Application/Others/Score.h
@@ -17,32 +17,34 @@ enum class Rank
 class Score
 {
 public:
-	static Score* GetInstance();
+    static Score* GetInstance();
 
-	void LoadTex();
+    void LoadTex();
 
-	//初期化
-	void Init();
-	//スコア追加
-	void Add(uint32_t enemyNum);
-	//スコア描画
-	void Draw();
-	void DrawHighScore();
-	void DrawRank(float rot);
-	void DrawImGui();
+    //初期化
+    void Init();
+    //スコア追加
+    void Add(uint32_t enemyNum);
+    //スコア描画
+    void Draw();
+    void DrawHighScore();
+    void DrawRank(float rot);
+    void DrawImGui();
 
-	//ハイスコアの更新(したか確認)
-	void HighScoreUpdate();
+    //ハイスコアの更新(したか確認)
+    void HighScoreUpdate();
 
-	//ハイスコア保存
-	void SaveScore();
-	//ハイスコア読み込み
-	void LoadScore();
+    //ハイスコア保存
+    void SaveScore();
+    //ハイスコア読み込み
+    void LoadScore();
 
-	void SetNowPos(const Vec2& pos) { nScorePos_ = pos; }
-	void SetHighPos(const Vec2& pos) { hScorePos_ = pos; }
-	void SetNowSize(float size) { nScoreSize_ = size; }
-	void SetHighSize(float size) { hScoreSize_ = size; }
+    void SetNowPos(const Vec2& pos) { nScorePos_ = pos; }
+    void SetHighPos(const Vec2& pos) { hScorePos_ = pos; }
+    void SetNowSize(float size) { nScoreSize_ = size; }
+    void SetHighSize(float size) { hScoreSize_ = size; }
+    void SetNowColor(const Vec4& color) { nScoreColor_ = color; }
+    void SetHighColor(const Vec4& color) { hScoreColor_ = color; }
 
 	void SetNowNum();	//座標とかサイズセットしたらこれ呼んで反映させる
 	void SetHighNum();	//座標とかサイズセットしたらこれ呼んで反映させる
@@ -53,6 +55,11 @@ public:
 	void SetRankPos(const Vec2& pos);
 	//指定されたUIの描画サイズ設定
 	void SetRankSize(float size);
+
+    DrawIntNumImage& GetDrawNum(void) { return drawNum_; }
+    DrawIntNumImage& GetDrawHighNum(void) { return drawNumHigh_; }
+
+    bool GetIsNewRecord(void) { return nowScore_ > highScore_; }
 
 private:
 	uint32_t nowScore_;		//現在のスコア
@@ -66,6 +73,9 @@ private:
 
 	float nScoreSize_;		//現在のスコアの描画サイズ
 	float hScoreSize_;		//今までのハイスコアの描画サイズ
+
+    Vec4 nScoreColor_;
+    Vec4 hScoreColor_;
 
 	Rank rank_;
 	Object obj_;
